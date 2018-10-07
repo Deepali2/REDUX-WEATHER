9419b826a0f0b514bd31dfd0ae8456367b4d26ae
Deepali2 <hdgarg@gmail.com>
2018-10-07 09:49:47 -0700

    sets up the action and the reducer; gets the details of the city weather using API keys

---
+++ src/actions/index.js
@@ -0,0 +1,14 @@
+import axios from 'axios';
+import {WEATHER_API_KEY} from '../config/weather';
+import {FETCH_WEATHER} from '../constants';
+const ROOT_URL = `https://api.openweathermap.org/data/2.5/forecast?appid=${WEATHER_API_KEY}`;
+
+export function fetchWeather(city) {
+  const url = `${ROOT_URL}&q=${city},us`;
+  const request = axios.get(url);
+  return {
+    type: FETCH_WEATHER,
+    payload: request
+  }
+}
+
