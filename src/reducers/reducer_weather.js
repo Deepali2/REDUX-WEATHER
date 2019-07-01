a0d00f815fa089df85c3ae179dc0f379ec2515dc
Deepali2 <hdgarg@gmail.com>
2019-07-01 16:31:08 -0700

    alerts the user if the API cannot find a city

--- src/reducers/reducer_weather.js
+++ src/reducers/reducer_weather.js
@@ -1,8 +1,11 @@
-import {FETCH_WEATHER} from '../constants';
+import { FETCH_WEATHER } from "../constants";
 
-
-export default function(state = [], action){
-  switch(action.type) {
+export default function(state = [], action) {
+  if (action.error) {
+    alert("City not found! Check city spelling or choose another city.");
+    return state;
+  }
+  switch (action.type) {
     case FETCH_WEATHER:
       // return [action.payload.data].concat(state);
       return [action.payload.data, ...state];
